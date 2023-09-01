8b4a8be0b6d5ca19f4d5af81d5f2e83c1ea78a6d
Azro1 <azro1.development@gmail.com>
2023-09-01 13:25:12 +0100

    Medication Details Template

--- src/pages/medication/Medication.js
+++ src/pages/medication/Medication.js
@@ -13,7 +13,45 @@ const { data: medication, isPending, error } = useFetch(`http://localhost:3000/m
        {isPending && <p className="loading">please wait...</p>}
        {error && <div className="error">{error}</div>}
        {medication && (
-        <div>{medication.name}</div>
+        <>
+        <h2 className="page-title">{medication.name}</h2>
+  
+        <div className="info">
+            <div className="dates">
+              <p>Start Date: <span>{medication.startDate}</span></p>
+              <p>End Date: <span>{medication.endDate}</span></p>
+            </div>
+
+            <section>
+              <p>Dosage: <span>{medication.dosage}</span></p>
+              <p>Type: <span>{medication.dosageForm}</span></p>
+              <p>To be taken: <span>{medication.frequency}</span></p>
+              <p>Administration: <span>{medication.adminRoute}</span></p>
+            </section>
+
+            <div className="instructions">
+              <p>Instructions: <span>{medication.instructions}</span></p>
+            </div>
+
+            <section>
+              <p>Prescribing Dr: <span>{medication.doctorName}</span></p>
+              <p>Email: <span>{medication.doctorEmail}</span></p>
+              <p>Pharmacy: <span>{medication.pharmacyName}</span></p> 
+              <p>Email: <span>{medication.pharmacyEmail}</span></p>
+            </section>
+
+            <section>
+              <p>Contains: {medication.contains.map((ingredient) =>(
+                    <span key={ingredient}>
+                        {ingredient}
+                    </span>
+                ))}
+                </p> 
+              <p>Side Effects: <span>{medication.sideEffects}</span></p>
+              <p>Advice: <span>{medication.warning}</span></p>
+            </section>
+          </div>
+        </>
        )}
     </div>
   )
