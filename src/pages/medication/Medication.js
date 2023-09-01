871aa7b45aa035ad42150ad55c5047e8517aa893
Azro1 <azro1.development@gmail.com>
2023-09-01 12:50:20 +0100

    Fetching a Single Medication

--- src/pages/medication/Medication.js
+++ src/pages/medication/Medication.js
@@ -1,10 +1,20 @@
+import { useParams } from 'react-router-dom'
+import useFetch from '../../hooks/useFetch'
+
 // styles
 import './Medication.css'
 
 const Medication = () => {
+const { id } = useParams()
+const { data: medication, isPending, error } = useFetch(`http://localhost:3000/medications/${id}`)
+
   return (
-    <div>
-        Medication
+    <div className="medication">
+       {isPending && <p className="loading">please wait...</p>}
+       {error && <div className="error">{error}</div>}
+       {medication && (
+        <div>{medication.name}</div>
+       )}
     </div>
   )
 }
