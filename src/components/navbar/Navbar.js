b12deb1f7ef72e2146b09c5fe5a9bf0b5a92c73f
Azro1 <azro1.development@gmail.com>
2023-09-01 20:34:35 +0100

    Accessing Context Values

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom'
 import storeLogo from '../../assets/logo.svg'
+
+// to use a context inside a component we use the useContext hook and also import our context (in this case it's our ThemeContext) 
+import { useContext } from 'react'
+import { ThemeContext } from '../../context/ThemeContext'
+
 import Searchbar from '../searchbar/Searchbar'
 
 // styles
 import './Navbar.css'
 
 const Navbar = () => {
+// we use the hook in our component to use a specific context by destructuring the properties from it and inside useContext we pass in whatever context we want to access
+const { color } = useContext(ThemeContext)
+
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }} >
         <nav>
           <Link to="/" className="storename">
             <h1>MediStore</h1>
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,19 +1,9 @@
-// CREATING A CONTEXT PROVIDER
-
-// this function allows us to create a new context object
 import { createContext } from 'react'
-
-// we invoke the createContext function which returns to us a new context object which we store in a const called ThemeContext 
 const ThemeContext = createContext() 
 
 const ThemeProvider = ({ children }) => {
-
-    
-    // custom logic
-
-
     return (
-        // on that context object is a context Provider component which we can then access by saying <ThemeContext.Provider> and this is the Provider component that would wrap any parts of our component tree to provide it with the value of our ThemeContext (index.js) - the value that we pass into the component tree is specified in a vlaue prop on the pProvider component
+      // ThemeContext which is providing a value to all of it's children
         <ThemeContext.Provider value={{ color: "blue"}} >
           { children }
         </ThemeContext.Provider>
