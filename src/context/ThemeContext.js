c77ea327d294534e99b51fc6b23a18187f9fe3e3
Azro1 <azro1.development@gmail.com>
2023-09-01 21:26:28 +0100

    Reducers & useReducer Part 2

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,39 +1,27 @@
-
-// Reducers & useReducer
-
-// A lot of the time when you're working with context and complex state and different ways of updating states you might want to reach for something called a reducer. A reducer is just a function and it's a way of working with with state (just like useState is a way of working with state) but a reducer makes it easier to work with multiple bits of related state that can be updated in serveral different ways. An example might be an array of data - where you can add to it - delete from it - update an item inside of it - sort it - filter it etc. There might be many ways you need to update that state and you might need varying logic to do it. Now you could do that using the useState hook and several different functions inside of the component to update the state depending on what you needed to do to it - but then it bloats the component with many functions and as our logic becomes a little more complex it becomes harder to manage - also you might want to perform multiple state changes at once, so you could end up with calling several different functions at once.. now this again become harder to manage in React and also our state updates become less reliable and less predictable.
-
-// So, to combat this we can use a reducer function - which is a single function that encapsulates all of the logic that we might need to update some state values. So it keeps everything together in one place and makes it easy to update state in multiple ways at once and it's a much more reliable way of handling slightly more complex state
-
-
-
-
 import { createContext, useReducer } from 'react'
-// the useReducer hook allows us to specify a reducer function which is going to be resposible for updating our state and keeping all of that update logic together in one place - and it also lets us specify an initial value as well
-
 const ThemeContext = createContext() 
 
-// reducer function which we declare outside of the component which will house all of our state logic
-const themeReducer = () => {
 
+
+// reducer function
+const themeReducer = (state, action) => {
+   switch(action.type) {
+
+   }
 }
 
 
-
-
 const ThemeProvider = ({ children }) => {
-
 const [state, dispatch] = useReducer(themeReducer, {
     color: "blue"
 })
-// We invoke the useReducer hook and pass in as the first argument the name of a function that we're going to use as our reducer function and as the second the initial state that we want to use. Now, just like the useState hook it return 2 values inside an array so we can capture them the same way
 
-// now these 2 values are:
 
-// 1. The state - which to begin with is going to be the initial state that we specified
-// 2. A dispatch function - the dispatch function is a way that we can dispatch the state change to the reducer function
+// custom changeColor function
+const changeColor = (color) => {
+    dispatch({ type: 'CHANGE_COLOR', payload: color })
+}
 
-// So if in the future we want to call the reducer function to change our state - we do that using the dispatch function.
 
     return (
         <ThemeContext.Provider value={{ color: "blue"}} >
@@ -43,3 +31,37 @@ const [state, dispatch] = useReducer(themeReducer, {
 }
 
 export {ThemeProvider, ThemeContext} 
+
+
+
+
+
+
+// Let's imagaine that we want to change this color value in the state .. how do we do that? 
+
+// well first of all i'm going to create a function called changeColor and that takes in a color argument to whatever color we want to change it to and then inside this function i'm just going to call the dispatch function.
+
+// Now the dispatch function takes in an object as an argument which is referred to as the dispatch action and on that action object we can specify 2 properties:
+
+// 1. type - the type property basically describes the type of state change that we want to make and normally the is going to be a string CAPITALISED - so this is the type of state change that we want to make
+
+// 2. payload - the payload is any data that we want to base the state change on and in our case we want to pass the new color as the payload value
+
+// So now we're calling this dispatch function and we're passing in the action object with these 2 properties on it. 
+
+// Now, in turn when we use this dispatch function - react looks at the reducer function associated with that dispatch and it finds our themeReducer function and then it fires that function to make the state change inside of it.
+
+
+
+// Now the themeReducer function - when it's called using the dispatch function takes in 2 arguments:
+
+// 1. state - the current up-to-date state
+// 2. action - the action object we passed into the dispatch call
+
+// So then we can use both of these 2 things to update the state. So all we need to do is return a value which represents the new state at the end. 
+
+// So for example if i just returned a string that said "hello" then our state value would be updated to be just a string that said "hello" but we don't want to do that .. we want to check the type of state change that we want to make and the return an updated state based on that
+
+// Now, we passed the type of state change that we want to make into the dispatch on the action object and we have access to that action object inside of the reducer function - so we can check that type property and react accordinly to it
+
+// So the way that we'll be doing this is by using a switch statement to check the type property of the action and then based on different types we'll return different values
