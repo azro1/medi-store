48ed49022df0365bedc1a2bf16eafe26fedf7348
Azro1 <azro1.development@gmail.com>
2023-09-01 14:12:34 +0100

    Making a Create Medication Form

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
-
-// styles
+import storeLogo from '../../assets/logo.svg'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <div className="navbar">
         <nav>
-          <Link to="/" className="storename"><h1>Medi Store</h1></Link>
+          <Link to="/" className="storename">
+            <h1>MediStore</h1>
+            <img src={storeLogo} alt="medication" />
+            </Link>
           <Link to="/create">Add Medication</Link>
         </nav>
     </div>
--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,12 +1,176 @@
 // styles
-import './Create.css'
+import { useState } from 'react';
+import './Create.css';
 
 const Create = () => {
-  return (
-    <div>
-        Create
-    </div>
-  )
-}
+  const [name, setName] = useState('');
+  const [dosage, setDosage] = useState('');
+  const [dosageForm, setDosageForm] = useState('');
+  const [frequency, setFrequency] = useState('');
+  const [adminRoute, setAdminRoute] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [doctorName, setDoctorName] = useState('');
+  const [doctorEmail, setDoctorEmail] = useState('');
+  const [pharmacyName, setPharmacyName] = useState('');
+  const [pharmacyEmail, setPharmacyEmail] = useState('');
+  // const [contains, setContains] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [storage, setStorage] = useState('');
+  const [sideEffects, setSideEffects] = useState('');
+  const [warning, setWarning] = useState('');
 
-export default Create
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, dosage, dosageForm);
+  };
+
+  return (
+    <form className='medication-form' onSubmit={handleSubmit}>
+      <h1 className='page-title'>Add a new Medication</h1>
+      <div className='form-control'>
+        <label>
+          <span>Name:</span>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Dosage:</span>
+          <input
+            type='text'
+            value={dosage}
+            onChange={(e) => setDosage(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Type:</span>
+          <input
+            type='text'
+            value={dosageForm}
+            onChange={(e) => setDosageForm(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Frequency:</span>
+          <input
+            type='text'
+            value={frequency}
+            onChange={(e) => setFrequency(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Admin Route:</span>
+          <input
+            type='text'
+            value={adminRoute}
+            onChange={(e) => setAdminRoute(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Start:</span>
+          <input
+            type='date'
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>End:</span>
+          <input
+            type='date'
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Doctor Name:</span>
+          <input
+            type='text'
+            value={doctorName}
+            onChange={(e) => setDoctorName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Email:</span>
+          <input
+            type='email'
+            value={doctorEmail}
+            onChange={(e) => setDoctorEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Pharmacy Name:</span>
+          <input
+            type='text'
+            value={pharmacyName}
+            onChange={(e) => setPharmacyName(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Email:</span>
+          <input
+            type='email'
+            value={pharmacyEmail}
+            onChange={(e) => setPharmacyEmail(e.target.value)}
+            required
+          />
+        </label>
+
+        {/* medication ingredients go here */}
+
+        <label>
+          <span>Instructions:</span>
+          <input
+            type='text'
+            value={instructions}
+            onChange={(e) => setInstructions(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Storage:</span>
+          <input
+            type='text'
+            value={storage}
+            onChange={(e) => setStorage(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Side Effects:</span>
+          <input
+            type='text'
+            value={sideEffects}
+            onChange={(e) => setSideEffects(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          <span>Advice:</span>
+          <input
+            type='text'
+            value={warning}
+            onChange={(e) => setWarning(e.target.value)}
+            required
+          />
+        </label>
+        <button className='form-btn'>Add</button>
+      </div>
+    </form>
+  );
+};
+
+export default Create;
