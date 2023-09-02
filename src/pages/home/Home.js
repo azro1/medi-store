d29941bcf07075dade1175f8bd7ae905f7530bea
Azro1 <azro1.development@gmail.com>
2023-09-02 11:01:17 +0100

    Fetching a Firestore Collection

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,7 @@
-import useFetch from '../../hooks/useFetch'
 import { useTheme } from '../../hooks/useTheme'
+import { useEffect, useState } from 'react'
+// import projectFirestore object from firebase config file
+import { projectFirestore } from '../../firebase/config'
 
 // styles
 import './Home.css'
@@ -8,8 +10,35 @@ import './Home.css'
 import MedicationList from '../../components/medicationlist/MedicationList'
 
 const Home = () => {
-  const { data, isPending, error } = useFetch('http://localhost:3000/medications')
   const { mode } = useTheme()
+  // same 3 properties that we're being returned from hook but now replaced using state
+  const [data, setData] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const [error, setError] = useState(false)
+
+  // we use useEffect to connect to firestore
+  useEffect(() => {
+    setIsPending(true)
+    // fetch data from firestore - collection connects to a collection in our database - to get all of the data from that collection we use a method called get - this fetches a snapshot of the collection and takes some time - it's asynchronous and returns a promise - this snapshot contains all of the documents inside the collection
+    projectFirestore.collection("medications").get().then((snapshot) => {
+      if (snapshot.empty) {
+        setError("No medications to load...")
+        setIsPending(false)
+      } else {
+        // for each document we're constructing a new object which consists of all of the data for that document and also the id (which we need as the key prop) and we're pushing that inside the results array 
+        let results = []
+        snapshot.docs.forEach((doc) => {
+          results.push({ id: doc.id, ...doc.data() })
+        })
+        // we update our local data to be the results
+        setData(results)
+        setIsPending(false)
+      }
+    }).catch((err) => {
+         setError(err.message)
+         setIsPending(false)
+    })
+  }, [])
 
   return (
     <div className="home">
