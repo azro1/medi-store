65bad78d77bd6c1dfd6ee1dc35b09062aca94bde
Azro1 <azro1.development@gmail.com>
2023-09-25 19:41:13 +0100

    Using the Login Hook

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useTheme } from '../../hooks/useTheme'
+import { useLogin } from '../../hooks/useLogin'
 
 // styles
 import './Login.css'
@@ -8,15 +9,17 @@ const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const { mode } = useTheme()
+  const { login, error, isPending } = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password)
+    login(email, password)
   }
 
   return (
     <form className={`login-form ${mode}`} onSubmit={handleSubmit} >
       <h2 className={`page-title ${mode}`}>Login</h2>
+      {error && <p className={`error ${mode}`}>{error}</p>}
       <div className="form-control">
         <label>
             <span>Email:</span>
@@ -26,7 +29,8 @@ const Login = () => {
             <span>Password:</span>
             <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
-        <button className="btn">login</button>
+        {!isPending && <button className="btn" >login</button>}
+        {isPending && <button className="btn" disabled>please wait...</button>}
       </div>
     </form>
   )
