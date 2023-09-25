c8ed8e086065ae7989f34a9957531dbc71e61562
Azro1 <azro1.development@gmail.com>
2023-09-25 16:53:00 +0100

    Firebase Authentication Setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 // firebase core
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDAtiJiCzzCRvl7-DbJqDoeNLv2o3aK8t0",
@@ -9,9 +10,13 @@ const firebaseConfig = {
     storageBucket: "medi-store-eef3b.appspot.com",
     messagingSenderId: "102430235092",
     appId: "1:102430235092:web:1b6991d34b84a154011e41"
-  };
+};
 
-  firebase.initializeApp(firebaseConfig)
-  const projectFirestore = firebase.firestore()
+// init app
+firebase.initializeApp(firebaseConfig)
 
-  export { projectFirestore }
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
