8cba57fc3d7b9ba4249b2df1017d06d1a937ad64
Azro1 <azro1.development@gmail.com>
2023-09-25 18:42:28 +0100

    Adding Signup Links and Using the Logout Hook

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,24 +2,36 @@ import { Link } from 'react-router-dom'
 import storeLogo from '../../assets/logo.svg'
 import Searchbar from '../searchbar/Searchbar'
 import { useTheme } from '../../hooks/useTheme'
+import { useLogout } from '../../hooks/useLogout'
 
 // styles
 import './Navbar.css'
 
 const Navbar = () => {
 const { color } = useTheme()
+const { logout } = useLogout()
 
   return (
     <div className="navbar" style={{ background: color }} >
         <nav>
+          
           <Link to="/" className="storename">
             <h1>MediStore</h1>
             <img src={storeLogo} alt="medication" />
             </Link>
+
             <div className="search-add">
               <Searchbar />
               <Link to="/create">Add Medication</Link>
             </div>
+            
+            <div className="signup">
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </div>
+
+            <button onClick={() =>logout()}>Logout</button>
+
         </nav>
     </div>
   )
